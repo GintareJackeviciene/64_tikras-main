6b6cd53b28b5c6b8d56e12f3ac0156c640642e49
Gintare <jgintare01@gmail.com>
2024-01-22 17:35:45 +0200

    router

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import NewItemPage from './pages/NewItemPage';
+import RegisterPage from './pages/RegisterPage';
+import ShopPage from './pages/ShopPage';
+import HomePage from './pages/HomePage';
+import Header from './components/layout/Header';
+
+export default function App() {
+  return (
+    <div className=''>
+     <Header/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
+}
